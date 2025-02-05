13d859bdefdeee0a8558ffcd6b4b4637d02b9afe
Jan Phandoh <janphandoh@gmail.com>
2025-02-05 07:51:43 +0000

    reverted some commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import Diagnose from "./components/Diagnose";
 import AuthContext from "./context";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
